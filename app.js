7f529990c359257f87d087da8744622c1e88e320
Ollieeryo <ollieeryo@gmail.com>
2022-11-04 02:37:34 +0800

    add category search function

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   // get same data with keyword, and force toLowerCase
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
+
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
