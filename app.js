5fc60f6d1705aad6043e3c433b67d02ae1ce18e9
Ollieeryo <ollieeryo@gmail.com>
2022-11-04 00:34:44 +0800

    Initialize Projects

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require handlebars
+const exphbs = require('express-handlebars')
+
+// require restaurant json
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// route setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results})
+})
+
+// show page setting and use params, and toLowerCase
+app.get('/restaurant/:restaurant_id', (req, res) => {
+  // get same id restaurant data
+  // change restaurant.id to string, same with req.params.restaurant.id
+  const restaurant = restaurantList.results.find(restaurant => 
+    restaurant.id.toString() === req.params.restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
+})
+
+// queryString setting
+app.get('/search', (req, res) => {
+  // get keyword
+  const keyword = req.query.keyword
+  // get same data with keyword, and force toLowerCase
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+
+// start and listen
+app.listen(port, () => {
+  console.log(`Express is listen localhost;${port}`)
+})
